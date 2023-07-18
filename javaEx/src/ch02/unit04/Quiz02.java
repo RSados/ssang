8ca1432c6fddf5979609eb8a6e994c3240f12990
sist <sist@DESKTOP-GIDB9LT>
2023-07-19 08:49:45 +0900

    sd

--- javaEx/src/ch02/unit04/Quiz02.java
+++ javaEx/src/ch02/unit04/Quiz02.java
@@ -13,7 +13,7 @@ public class Quiz02 {
 			System.out.print("한문자 ? ");
 			b = sc.nextInt();//한문자 입력
 			System.out.printf(" %c",b);//%c가 아스키 코드로 변환하여 알려줌
-			System.out.print((char)b);
+			System.out.print((char)b);//dsa
 			
 			sc.close();
 	}
