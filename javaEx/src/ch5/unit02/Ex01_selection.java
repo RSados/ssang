f68d3572c753e6159e450a682a4709448824d65c
sist <sist@DESKTOP-GIDB9LT>
2023-07-26 14:59:09 +0900

    day8

---
+++ javaEx/src/ch5/unit02/Ex01_selection.java
@@ -0,0 +1,34 @@
+package ch5.unit02;
+
+import java.util.Arrays;
+
+public class Ex01_selection {
+	public static void main(String[] args) {
+		//selection sort
+		int[]num = new int[] {15,2,13,8,12};
+		
+		System.out.print("Source Data:");
+		for(int n : num) {
+			System.out.print(n+" ");
+			
+		}
+		System.out.println();
+		
+		//Sort
+		int t;
+		for(int i=0;i<num.length-1;i++) {
+			for(int j=i+1;j<num.length;j++) {
+				if(num[i]>num[j]) {
+					t=num[i];num[i]=num[j];num[j]=t;
+				}
+			}
+			//System.out.println((i+1)+"회전: "+Arrays.toString(num));
+		}
+		System.out.print("Sort Data: ");
+		for(int n : num) {
+			System.out.print(n+ " ");}
+			System.out.println();
+		
+	}
+
+}
---
+++ javaEx/src/ch5/unit02/Ex02_bubble.java
@@ -0,0 +1,44 @@
+package ch5.unit02;
+
+public class Ex02_bubble {
+	public static void main(String[] args) {
+		//Bubble Sort
+		int num[]=new int[] {17,13,14,16,15};
+		int t;
+		
+		System.out.print("Source Data: ");
+		for(int n:num) {
+			System.out.print(n+" ");}
+			
+	     //Sort
+		 //1회전: (0:1)(1:2)(2:3)(3:4)
+		 //2회전: (1:2)(2:3)(3:4)
+		 //3회전: (2:3)(3:4)
+		 //4회전: (3:4)
+		for(int i=1;i<num.length-1;i++) {
+			for(int j=0;j<num.length-1;j++) {
+				if(num[j]>num[j+1]) {
+					t=num[j];num[j]=num[j+1];num[j+1]=t;
+				}
+				
+			}
+		}
+		
+		
+		
+		
+		
+		System.out.println();
+		
+		System.out.print("Sort Data: ");
+		for(int n:num) {
+			System.out.print(n+" ");
+			}
+		System.out.println();
+		
+		
+		
+		
+	}
+
+}
